0194d0c3c11d6db21e7ef23bc81ccd6dcc09e219
M-Nowfal <nowfalmmuhammed@gmail.com>
2026-02-11 09:03:56 +0530

    next.config.ts updated

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: [
     "pdf-parse",
-    "@huggingface/transformers",
   ],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "onnxruntime-node": false,
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
